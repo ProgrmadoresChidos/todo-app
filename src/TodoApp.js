edd1bd2c51ee2f8ca3c8024f0f239eb1381af992
Roberto Valdez <robert.tito94@gmail.com>
2020-12-04 21:08:43 -0600

    subir proyecto

---
+++ src/TodoApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TodoApp = () => {
+    return (
+        <div>
+            todo works
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/styles.scss';
+import { TodoApp } from './TodoApp';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TodoApp />,
   document.getElementById('root')
 );
 
