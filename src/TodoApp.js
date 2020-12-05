ffeabb9567ac0c01743b51ca08af3cd6baa13da2
Roberto Valdez <robert.tito94@gmail.com>
2020-12-04 22:15:46 -0600

    context form

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { AddNewTodo } from './components/AddNewTodo'
+import { TodoContext } from './context/TodoContext';
+import { todoReducer } from './reducer/todoReducer'
 
 export const TodoApp = () => {
+
+    const initTodos = [
+        {
+            id: new Date().getDate(),
+            desc: 'test',
+            active: true
+        }
+    ]
+
+    const [todos, dispatch] = useReducer(todoReducer, initTodos);
+
     return (
-        <div>
-            todo works
-        </div>
+        <TodoContext.Provider value={{
+            todos,
+            dispatch
+        }}>
+            <div>
+                todo works
+                <AddNewTodo />
+            </div>
+        </TodoContext.Provider>
     )
 }
--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { addTodo } from '../actions/todo';
+import { TodoContext } from '../context/TodoContext'
 
 export const AddNewTodo = () => {
+
+    const { todos, dispatch } = useContext(TodoContext);
+    const [formValue, setFormValue] = useState({
+        inputTodo: ''
+    });
+    const { inputTodo } = formValue;
+
+    useEffect(() => {
+        console.log(todos);
+    }, [todos]);
+
+    const handleInput = ({ target }) => {
+        setFormValue({
+            ...formValue,
+            [target.name]: target.value
+        });
+    }
+
+    const hadleAddTodo = (e) => {
+        e.preventDefault();
+        const newTodo = {
+            id: new Date().getTime(),
+            desc: inputTodo,
+            active: true
+        };
+        dispatch(addTodo(newTodo));
+    }
+
     return (
         <div className="addtodo">
             <div className="addtodo__form">
-                <input type="text" placeholder="add deatils" className="addtodo__input" />
-                <button className="addtodo__button" >Add</button>
+                <form onSubmit={hadleAddTodo}>
+                    <input
+                        onChange={handleInput}
+                        value={inputTodo}
+                        name="inputTodo"
+                        type="text"
+                        placeholder="add deatils"
+                        className="addtodo__input" />
+                    <button type="submit" className="addtodo__button" >Add</button>
+                </form>
             </div>
         </div>
     )
